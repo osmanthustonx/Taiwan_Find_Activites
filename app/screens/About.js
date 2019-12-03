5da046eb5c76e55dcdbb9fd468b79f5bb080a596
Kuei <vx3012564897@gmail.com>
2019-12-03 16:51:41 +0800

    Calendar 的使用及權限套件測試成功

--- app/screens/About.js
+++ app/screens/About.js
@@ -2,6 +2,63 @@ import React from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 import {Actions} from 'react-native-router-flux';
 
+import * as AddCalendarEvent from 'react-native-add-calendar-event';
+import moment from 'moment';
+
+const utcDateToString = (momentInUTC: moment): string => {
+  let s = moment.utc(momentInUTC).format('YYYY-MM-DDTHH:mm:ss.SSS[Z]');
+  // console.warn(s);
+  return s;
+};
+
+export default class About extends React.Component {
+  render() {
+    const nowUTC = moment.utc();
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>About {this.props.user}</Text>
+        <Text>
+          date:{' '}
+          {moment
+            .utc(nowUTC)
+            .local()
+            .format('lll')}
+        </Text>
+
+        <Button
+          onPress={() => {
+            About.addToCalendar(this.props.user, nowUTC);
+          }}
+          title="Add to calendar"
+        />
+
+        <Button onPress={Actions.pop} title="Back" />
+      </View>
+    );
+  }
+  static addToCalendar = (title, startDateUTC) => {
+    const eventConfig = {
+      title,
+      startDate: utcDateToString(startDateUTC),
+      endDate: utcDateToString(moment.utc(startDateUTC).add(1, 'hours')),
+      notes: 'dddddd!',
+    };
+
+    AddCalendarEvent.presentEventCreatingDialog(eventConfig)
+      .then(eventInfo => {
+        // handle success - receives an object with `calendarItemIdentifier` and `eventIdentifier` keys, both of type string.
+        // These are two different identifiers on iOS.
+        // On Android, where they are both equal and represent the event id, also strings.
+        // when { action: 'CANCELED' } is returned, the dialog was dismissed
+        console.warn(JSON.stringify(eventInfo));
+      })
+      .catch(error => {
+        // handle error such as when user rejected permissions
+        console.warn(error);
+      });
+  };
+}
+
 var styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -20,14 +77,3 @@ var styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
-
-export default class About extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>About {this.props.user}</Text>
-        <Button onPress={Actions.pop} title="Back" />
-      </View>
-    );
-  }
-}
