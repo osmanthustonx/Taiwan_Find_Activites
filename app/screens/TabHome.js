74dd0b942fab338fbc022f4b5c57995d30a73633
Kuei <vx3012564897@gmail.com>
2019-12-04 10:49:16 +0800

    Date Picker 套件測試成功

--- app/screens/TabHome.js
+++ app/screens/TabHome.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, Platform} from 'react-native';
 import {Actions} from 'react-native-router-flux';
 
 var styles = StyleSheet.create({
@@ -21,12 +21,51 @@ var styles = StyleSheet.create({
   },
 });
 
+import DateTimePicker from '@react-native-community/datetimepicker';
+
 export default class Home extends React.Component {
   /** For Drawer Sample
   componentDidMount() {
     Actions.drawerOpen();
   }
   */
+
+  state = {
+    date: new Date(),
+    mode: 'date',
+    show: false,
+  };
+
+  setDate = (event, date) => {
+    date = date || this.state.date;
+
+    this.setState({
+      show: Platform.OS === 'ios' ? true : false,
+      date,
+    });
+  };
+
+  show = mode => {
+    this.setState({
+      show: true,
+      mode,
+    });
+  };
+
+  unShow = mode => {
+    this.setState({
+      show: false,
+      mode,
+    });
+  };
+
+  datepicker = () => {
+    this.show('date');
+  };
+
+  timepicker = () => {
+    this.show('time');
+  };
 
   onPress = () => {
     Actions[this.props.keyPrefix + '_about']({
@@ -35,11 +74,31 @@ export default class Home extends React.Component {
   };
 
   render() {
+    const {show, date, mode} = this.state;
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome {this.props.title}</Text>
         <Button onPress={this.onPress} title="Go to About" />
         <Button title="Back" onPress={Actions.pop} />
+
+        <View style={{width: '100%'}}>
+          <View>
+            <Button onPress={this.datepicker} title="Show date picker!" />
+          </View>
+          <View>
+            <Button onPress={this.timepicker} title="Show time picker!" />
+          </View>
+          {show && (
+            <DateTimePicker
+              value={date}
+              mode={mode}
+              is24Hour={true}
+              display="default"
+              onChange={this.setDate}
+            />
+          )}
+          <Button title={'確定'} onPress={this.unShow} />
+        </View>
       </View>
     );
   }
