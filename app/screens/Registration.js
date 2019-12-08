c0c4dffd177ea33590c4722a9536ee68f0ff368a
Kuei <vx3012564897@gmail.com>
2019-12-09 01:09:28 +0800

    註冊頁面API接完，但還有後端回應為“應該有欄位沒有寫唷”

--- app/screens/Registration.js
+++ app/screens/Registration.js
@@ -9,14 +9,25 @@ import RadioForm, {
   RadioButtonLabel,
 } from 'react-native-simple-radio-button';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import moment from 'moment';
 
 export default class Login extends React.Component {
   state = {
-    email: '',
-    value: 0,
+    value: null,
     date: new Date(),
-    mode: 'date',
     show: false,
+    // memberData: {
+    //   name: '',
+    //   email: '',
+    //   birth: '',
+    //   gender: 0,
+    //   password: '',
+    // },
+    name: '',
+    email: '',
+    birth: new Date(),
+    gender: 0,
+    password: '',
   };
   onPress = () => {
     Actions.registered({user: '花的世界'});
@@ -33,59 +44,129 @@ export default class Login extends React.Component {
       console.log('Email is Correct');
     }
   };
-  radio_props = [{label: '女', value: 0}, {label: '男', value: 1}];
 
+  /*------input資料處理------*/
+
+  //name
+  onChangeName = name => {
+    this.setState({
+      name,
+    });
+    // console.log(this.state.name);
+  };
+
+  //gender
+  radio_props = [{label: '女', value: 0}, {label: '男', value: 1}]; //列舉
+  onPressGender = gender => {
+    this.setState({
+      gender,
+    });
+    // console.log(this.state.memberData);
+  };
+
+  //birth
+  datepicker = () => {
+    this.setState({
+      show: !this.state.show,
+    });
+  };
+
+  //一定要有這個event參數，不知為何
+  onChangeBirth = (event, birth) => {
+    birth = birth || this.state.birth;
+
+    this.setState({
+      birth,
+    });
+    // console.log(this.state.memberData);
+  };
   setDate = (event, date) => {
     date = date || this.state.date;
 
     this.setState({
-      show: Platform.OS === 'ios' ? true : false,
+      // show: Platform.OS === 'ios' ? true : false,
       date,
     });
   };
 
-  show = mode => {
+  // email
+  onChangeEmail = email => {
     this.setState({
-      show: true,
-      mode,
+      email,
     });
+    // console.log(this.state.memberData);
   };
 
-  unShow = mode => {
+  //password
+  onChangePassword = password => {
     this.setState({
-      show: false,
-      mode,
+      password,
     });
+    // console.log(this.state.memberData);
   };
 
-  datepicker = () => {
-    this.show('date');
+  /*------撈API資料------*/
+
+  goRegist = () => {
+    let data = {
+      Name: this.state.name,
+      Email: this.state.email,
+      // Birth: moment(this.state.date).format('ll'),
+      Birth: '2000-01-02',
+      Gender: this.state.gender,
+      Password: this.state.password,
+    };
+    let xhr = new XMLHttpRequest();
+    // xhr.withCredentials = true;
+
+    xhr.addEventListener('readystatechange', function() {
+      if (this.readyState === 4) {
+        console.log(this.responseText);
+        console.log(JSON.stringify(data));
+      }
+    });
+
+    xhr.open('POST', 'https://tfa.rocket-coding.com/Member/AddMember');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(JSON.stringify(data));
   };
 
-  timepicker = () => {
-    this.show('time');
+  test = () => {
+    let data = {
+      Name: this.state.name,
+      Email: this.state.email,
+      Birth: moment(this.state.date).format('ll'),
+      Gender: this.state.gender,
+      Password: this.state.password,
+    };
+    console.log(data);
   };
+
   render() {
     return (
       <View style={styles.container}>
+        {/* 姓名Input */}
         <Input
           placeholder="姓名"
           leftIcon={<Icon name="ios-person" size={24} color="black" />}
           leftIconContainerStyle={{paddingRight: 10}}
-          errorStyle={{color: 'red'}}
-          errorMessage="ENTER A VALID ERROR HERE"
+          // errorStyle={{color: 'red'}}
+          // errorMessage="ENTER A VALID ERROR HERE"
+          onChangeText={this.onChangeName}
         />
+
+        {/* 性別Input */}
         <View>
           <RadioForm
             radio_props={this.radio_props}
             initial={0}
             formHorizontal={true}
             labelHorizontal={false}
-            onPress={value => {
-              this.setState({value: value});
-            }}
+            onPress={this.onPressGender}
           />
         </View>
+
+        {/* 生日Input */}
         <View style={{width: '100%'}}>
           <View>
             <Button onPress={this.datepicker} title="生日" type="outline" />
@@ -93,34 +174,41 @@ export default class Login extends React.Component {
           {this.state.show && (
             <DateTimePicker
               value={this.state.date}
-              mode={this.state.mode}
+              mode={'date'}
               is24Hour={true}
               display="default"
               onChange={this.setDate}
             />
           )}
-          <Button title={'生日確定'} onPress={this.unShow} type="clear" />
         </View>
 
+        {/* 信箱Input */}
         <Input
           placeholder="請輸入電子信箱"
           leftIcon={<Icon name="ios-mail" size={24} color="black" />}
           leftIconContainerStyle={{paddingRight: 10}}
-          errorStyle={{color: 'red'}}
-          errorMessage="ENTER A VALID ERROR HERE"
+          // errorStyle={{color: 'red'}}
+          // errorMessage="ENTER A VALID ERROR HERE"
+          onChangeText={this.onChangeEmail}
         />
+
+        {/* 密碼Input */}
         <Input
           placeholder="請輸入密碼"
           leftIcon={<Icon name="ios-lock" size={24} color="black" />}
           leftIconContainerStyle={{paddingRight: 10}}
-          errorStyle={{color: 'red'}}
-          errorMessage="ENTER A VALID ERROR HERE"
+          // errorStyle={{color: 'red'}}
+          // errorMessage="ENTER A VALID ERROR HERE"
+          onChangeText={this.onChangePassword}
+          secureTextEntry={true}
         />
 
         <Button
           title="送出"
           onPress={() => {
-            Actions.pop();
+            // Actions.pop();
+            this.goRegist();
+            // this.test();
           }}
         />
       </View>
