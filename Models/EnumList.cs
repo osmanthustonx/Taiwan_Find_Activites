02e4c287ca4f6df93f898eed6f887ba6c7165a85
JingRui0607 <phrost0428123@gmail.com>
2020-01-16 14:15:36 +0800

    create back_end

---
+++ Models/EnumList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ActiviTW.Models
+{
+    public class EnumList
+    {
+        public enum EnableTyoe
+        {
+            開 = 1,
+            關 = 0
+        }
+        public enum BoolinType
+        {
+            是 = 1,
+            否 = 0
+        }
+        public enum GenderType
+        {
+            女,
+            男
+        }
+    }
+}
---
+++ Models/Member.cs
@@ -0,0 +1,57 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using static ActiviTW.Models.EnumList;
+
+namespace ActiviTW.Models
+{
+    public class Member
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0}必填")]
+        [MaxLength(50)]
+        [Display(Name = "姓名")]
+        public string Name { get; set; }
+
+
+        [Display(Name = "生日")]
+        public DateTime Birth { get; set; }
+
+        [JsonIgnore] //不會轉成json格式
+        [Display(Name = "性別")]
+        public GenderType Gender { get; set; }
+
+        [Required(ErrorMessage = "{0}必填")]
+        [EmailAddress(ErrorMessage = "{0}格式錯誤")]
+        [MaxLength(200)]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [MaxLength(500)]
+        [Display(Name = "密碼")]
+        public string PasswordSalt { get; set; }
+
+        [Display(Name = "照片")]//改欄位名稱
+        [MaxLength]//資料型態長度
+        public string Image { get; set; }
+
+
+        [Display(Name = "建立日期")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        //[DisplayFormat(ApplyFormatInEditMode = true,DataFormatString = "{0:yyyy年MM月dd日}")]
+        public DateTime? InitDate { get; set; }
+       
+    }
+}
